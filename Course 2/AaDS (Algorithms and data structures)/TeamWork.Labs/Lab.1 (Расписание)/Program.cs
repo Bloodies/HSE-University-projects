b42d48d8dff425b5cb2048fdfa3ade7d71f7c6d7
Bloodies <eschepokov@mail.ru>
2019-10-11 01:14:50 +0500

    Initial commit

---
+++ Course 2/AaDS (Algorithms and data structures)/TeamWork.Labs/Lab.1 (Расписание)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._1__Расписание_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
