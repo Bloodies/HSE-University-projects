45185fd694459455053e4bf594a1423ade17093b
Bloodies <eschepokov@mail.ru>
2019-10-11 07:16:59 +0500

    Initial commit

---
+++ Course 2/AaDS (Algorithms and data structures)/Algorithms Practice/Week.4/Последовательность (Sequence)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Последовательность__Sequence_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
