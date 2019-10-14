454d6e4b1f605084046a1b970f7f0b4ac97668fc
Bloodies <eschepokov@mail.ru>
2019-10-14 21:12:07 +0500

    Initial commit

---
+++ Course 2/AaDS (Algorithms and data structures)/Algorithms Practice/Week.5/1. Bunch (Куча)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Bunch__Куча_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
