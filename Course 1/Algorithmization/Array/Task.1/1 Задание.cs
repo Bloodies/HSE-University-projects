1ceffa34b1b1493f5b8d819951f042a78e9a1eb5
Bloodies <eschepokov@mail.ru>
2019-10-30 22:56:57 +0500

    Finished tasks with arrays

---
+++ Course 1/Algorithmization/Array/Task.1/1 Задание.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Задание:
+ * Используя датчик случайных чисел, заполнить массив из двадцати элементов неповторяющимися числами.
+ */
+
+namespace Task._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int elem;
+            bool ok;
+            int[] array = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                do
+                {
+                    elem = rnd.Next(1, 100);
+                    ok = true;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (elem == array[j])
+                        {
+                            ok = false;
+                        }
+                    }
+                } while (!ok);
+                array[i] = elem;
+            }
+            Console.WriteLine("------------------Массив-----------------\n");
+            foreach (var i in array)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Ещё Массив? [да/нет]");
+            string rpt = Console.ReadLine();
+            Console.WriteLine("");
+            if (rpt == "да")
+            {
+                Main();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
