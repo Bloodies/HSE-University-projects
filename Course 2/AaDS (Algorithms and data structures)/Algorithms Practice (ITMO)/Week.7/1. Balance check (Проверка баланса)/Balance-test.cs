3a6bb18c5c8a1340543f6ae0cd9a713d4cd1ce66
Bloodies <eschepokov@mail.ru>
2019-11-10 14:30:17 +0500

    Initial commit

--- Course 2/AaDS (Algorithms and data structures)/Algorithms Practice (ITMO)/Week.7/1. Balance check (Проверка баланса)/Balance-test.cs
+++ Course 2/AaDS (Algorithms and data structures)/Algorithms Practice (ITMO)/Week.7/1. Balance check (Проверка баланса)/Balance-test.cs
@@ -10,7 +10,6 @@ namespace _1.Balance_check__Проверка_баланса_
     class Balance_test
     {
         private static (int, int, int)[] nodes;
-
         private static StreamWriter output = new StreamWriter("output.txt");
 
         private static void BigStackMain()
@@ -20,10 +19,8 @@ namespace _1.Balance_check__Проверка_баланса_
             {
                 output.WriteLine(NodeBalance(i));
             }
-
             output.Close();
         }
-
         private static int Height_tree(int numberNode)
         {
             int leftHeight = 0, rightHeight = 0;
@@ -31,32 +28,27 @@ namespace _1.Balance_check__Проверка_баланса_
             {
                 leftHeight = Height_tree(nodes[numberNode].Item1 - 1);
             }
-
             if (nodes[numberNode].Item2 != 0)
             {
                 rightHeight = Height_tree(nodes[numberNode].Item2 - 1);
             }
-
             nodes[numberNode].Item3 = Math.Max(leftHeight, rightHeight) + 1;
             return Math.Max(leftHeight, rightHeight) + 1;
         }
-
         public static int NodeBalance(int numberNode)
         {
-            int leftHeight = 0, rightHeight = 0;
+            int leftHeight = 0, 
+                rightHeight = 0;
             if (nodes[numberNode].Item1 != 0)
             {
                 leftHeight = nodes[nodes[numberNode].Item1 - 1].Item3;
             }
-
             if (nodes[numberNode].Item2 != 0)
             {
                 rightHeight = nodes[nodes[numberNode].Item2 - 1].Item3;
             }
-
             return rightHeight - leftHeight;
         }
-
         static void Main()
         {
             StreamReader input = new StreamReader("input.txt");
@@ -68,7 +60,6 @@ namespace _1.Balance_check__Проверка_баланса_
                 tokens = input.ReadLine().Split(' ');
                 nodes[i] = (int.Parse(tokens[1]), int.Parse(tokens[2]), 0);
             }
-
             input.Close();
             Thread newThread = new Thread(BigStackMain, 100000000);
             newThread.Start();
--- Course 2/AaDS (Algorithms and data structures)/Algorithms Practice (ITMO)/Week.7/1. Balance check (Проверка баланса)/Balance.cs
+++ Course 2/AaDS (Algorithms and data structures)/Algorithms Practice (ITMO)/Week.7/1. Balance check (Проверка баланса)/Balance.cs
@@ -2,15 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.IO;
 
 namespace _1.Balance_check__Проверка_баланса_
 {
     class Balance
     {
+        private static (int, int, int)[] nodes;
+        //private static StreamWriter output = new StreamWriter("output.txt");
+
+        private static void BigStackMain()
+        {
+            Height_tree(0);
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                /*output.*/Console.WriteLine(NodeBalance(i));
+            }
+            Console.ReadKey();
+            //output.Close();
+        }
+        private static int Height_tree(int numberNode)
+        {
+            int leftHeight = 0, rightHeight = 0;
+            if (nodes[numberNode].Item1 != 0)
+            {
+                leftHeight = Height_tree(nodes[numberNode].Item1 - 1);
+            }
+
+            if (nodes[numberNode].Item2 != 0)
+            {
+                rightHeight = Height_tree(nodes[numberNode].Item2 - 1);
+            }
+
+            nodes[numberNode].Item3 = Math.Max(leftHeight, rightHeight) + 1;
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+        public static int NodeBalance(int numberNode)
+        {
+            int leftHeight = 0, 
+                rightHeight = 0;
+            if (nodes[numberNode].Item1 != 0)
+            {
+                leftHeight = nodes[nodes[numberNode].Item1 - 1].Item3;
+            }
+            if (nodes[numberNode].Item2 != 0)
+            {
+                rightHeight = nodes[nodes[numberNode].Item2 - 1].Item3;
+            }
+            return rightHeight - leftHeight;
+        }
         static void Main()
         {
+            //StreamReader input = new StreamReader("input.txt");
+            int N_length = int.Parse(/*input.*/Console.ReadLine());
+            nodes = new (int, int, int)[N_length];
+            string[] tokens;
+            for (int i = 0; i < N_length; i++)
+            {
+                tokens = /*input.*/Console.ReadLine().Split(' ');
+                nodes[i] = (int.Parse(tokens[1]), int.Parse(tokens[2]), 0);
+            }
+            //input.Close();
+            Thread newThread = new Thread(BigStackMain, 100000000);
+            newThread.Start();
         }
     }
 }
