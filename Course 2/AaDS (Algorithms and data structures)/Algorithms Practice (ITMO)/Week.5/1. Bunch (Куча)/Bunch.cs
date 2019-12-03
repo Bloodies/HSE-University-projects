6daccb239deb9296a69aa51561943132b254e024
Bloodies <eschepokov@mail.ru>
2019-12-03 20:27:28 +0500

    Initial commit

---
+++ Course 2/AaDS (Algorithms and data structures)/Algorithms Practice (ITMO)/Week.5/1. Bunch (Куча)/Bunch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Bunch__Куча_
+{
+    class Bunch
+    {
+    }
+}
