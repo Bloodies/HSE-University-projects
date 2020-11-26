830d27d9cc85c0d2f3572a4a15cc06735e548ee8
Bloodies <eschepokov@mail.ru>
2020-11-26 19:04:14 +0500

    /

--- Course 3/Computer.graphics/Graphic redactor (version cs)/Graphic redactor/src/Window.cs
+++ Course 3/Computer.graphics/Graphic redactor (version cs)/Graphic redactor/src/Window.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Graphic_redactor
+namespace Graphic_redactor.src
 {
-    public partial class Form1 : Form
+    public partial class Window : Form
     {
-        public Form1()
+        public Window()
         {
             InitializeComponent();
         }
