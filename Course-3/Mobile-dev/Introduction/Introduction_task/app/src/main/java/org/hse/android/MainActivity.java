0fd9fe490f01d029af42e139f5d61444b30641a3
Bloodies <eschepokov@mail.ru>
2021-02-02 22:08:42 +0500

    File update

---
+++ Course-3/Mobile-dev/Introduction/Introduction_task/app/src/main/java/org/hse/android/MainActivity.java
@@ -0,0 +1,15 @@
+package org.hse.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("STRING", "Строчка с моим именем и фамилией: Чепоков Елизар");
+        setContentView(R.layout.activity_main);
+    }
+}
