185b2a636a39f493fd7b18d4fe8e64543e6678f4
Bloodies <eschepokov@mail.ru>
2021-02-04 19:06:31 +0500

    add workflow form course

---
+++ Course-3/Mobile-dev/Stepik/3_architecture/3-3/3-3-10/app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.company.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.company.lib.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Write your code here
+
+    }
+}
---
+++ Course-3/Mobile-dev/Stepik/3_architecture/3-3/3-3-9/app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.company.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("CHECKER", this.toString())
+    }
+}
