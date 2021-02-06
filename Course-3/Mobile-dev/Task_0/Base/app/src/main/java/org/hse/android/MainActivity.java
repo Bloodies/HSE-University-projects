9a3a6caf4fa47107e889dcbdf0d4a0dd7aaad7fc
Bloodies <eschepokov@mail.ru>
2021-02-06 21:39:09 +0500

    comleted task 0

--- Course-3/Mobile-dev/Task_0/Base/app/src/main/java/org/hse/android/MainActivity.java
+++ Course-3/Mobile-dev/Task_0/Base/app/src/main/java/org/hse/android/MainActivity.java
@@ -1,8 +1,9 @@
 package org.hse.android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().show();
+
+        View button_student = findViewById(R.id.button_student);
+        View button_teacher = findViewById(R.id.button_teacher);
+
+        button_student.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { Toast.makeText(MainActivity.this, "РАСПИСАНИЕ ДЛЯ СТУДЕНТОВ", Toast.LENGTH_SHORT).show(); }
+        });
+        button_teacher.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) { Toast.makeText(MainActivity.this, "РАСПИСАНИЕ ДЛЯ ПРЕПОДОВАТЕЛЯ", Toast.LENGTH_SHORT).show(); }
+        });
     }
 }
