8bba55d7cefa0705544f296515f4e5f72a3b48ae
Bloodies <eschepokov@mail.ru>
2021-02-17 16:30:17 +0500

    upd

--- Course-3/Mobile-dev/Task_2/Base/app/src/main/java/org/hse/android/SettingsActivity.java
+++ Course-3/Mobile-dev/Task_2/Base/app/src/main/java/org/hse/android/SettingsActivity.java
@@ -1,7 +1,18 @@
 package org.hse.android;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
