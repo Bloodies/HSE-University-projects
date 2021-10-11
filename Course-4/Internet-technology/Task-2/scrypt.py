e6dee7c94a74140f116e098d8b17f1710e135cd2
Bloodies <eschepokov@mail.ru>
2021-10-11 14:44:23 +0500

    add task 1 and task 2
    added tasks for internet tech course

---
+++ Course-4/Internet-technology/Task-2/scrypt.py
@@ -0,0 +1,55 @@
+# Парсинг сайта booking.com
+import requests
+import re
+from pprint import pprint
+from bs4 import BeautifulSoup as bs
+
+api_key = r"0f77ff2a5caad795bbdaffcfd968d8a6"
+
+url_1 = r"https://ru.wikipedia.org/w/index.php?title=JSON&action=history"
+url_2 = r"https://api.hh.ru/vacancies?industry=7&per_page=10&page=0"
+url_3 = r"https://www.booking.com/searchresults.ru.html?label=gen173nr-1FCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEU2AEB6AEB-AELiAIBqAIDuAKgxYXuBcACAQ&sid=bc5edb21df74621477bf9c71bcf97f49&sb=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.ru.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEU2AEB6AEB-AELiAIBqAIDuAKgxYXuBcACAQ%3Bsid%3Dbc5edb21df74621477bf9c71bcf97f49%3Bsb_price_type%3Dtotal%26%3B&ss=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C%2C+%D0%9F%D0%B5%D1%80%D0%BC%D1%81%D0%BA%D0%B8%D0%B9+%D0%BA%D1%80%D0%B0%D0%B9%2C+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&is_ski_area=&ssne=%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD&ssne_untouched=%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD&checkin_year=2019&checkin_month=11&checkin_monthday=9&checkout_year=2019&checkout_month=11&checkout_monthday=10&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=gthvm&ac_position=0&ac_langcode=ru&ac_click_type=b&dest_id=-2980155&dest_type=city&iata=PEE&place_id_lat=58.01496&place_id_lon=56.24672&search_pageview_id=089a5390e2b60096&search_selected=true&search_pageview_id=089a5390e2b60096&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0"
+
+
+def Task_1():
+    ip = r"91.235.175.82"
+    request = requests.get(r"http://api.ipstack.com/" + ip + "?access_key=" + api_key)
+    obj = request.json()
+    print(obj["country_name"])
+
+
+def Task_2():
+    request = requests.get(url_2)
+    pprint(request.json())
+
+
+def Task_3():
+    while True:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+        r = requests.get(url_3, headers=headers)
+        soup = bs(r.content, "html.parser")
+
+        hotelBlock = soup.find_all("div", {"class": "sr_item"})
+
+        i = 0
+        for hotel in hotelBlock:
+            hotelName = hotel.find("span", {"class": "sr-hotel__name"}).get_text().strip()
+            print(hotelName)
+
+            print("\n Элементов на странице:" + " " + str(len(hotelBlock)))
+
+            nextPage = soup.find("a", title="Next page")
+            if nextPage is None:
+                nextPage = soup.find("a", title="Следующая страница")
+
+            if nextPage is not None:
+                url = "https://www.booking.com/" + nextPage.attrs["href"]
+                print("Обработка следующей страницы... \n")
+            else:
+                break
+
+if __name__ == '__main__':
+    Task_1()
+    # Task_2()
+    # Task_3()
