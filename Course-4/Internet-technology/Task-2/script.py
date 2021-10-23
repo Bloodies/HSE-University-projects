d754d6cc8c19ce9e85bcc4f20e6b7b9bdd350688
Bloodies <eschepokov@mail.ru>
2021-10-23 15:31:50 +0500

    complited task 2.3

--- Course-4/Internet-technology/Task-2/script.py
+++ Course-4/Internet-technology/Task-2/script.py
@@ -2,16 +2,15 @@
 
 import re
 import requests
-from pprint import pprint
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
-api_key = r""
+api_key = r''
 
-url_1 = r"https://ru.wikipedia.org/w/index.php?title=JSON&action=history"
-# url_2 = r"https://api.hh.ru/vacancies?industry=7&per_page=10&page=0"
-url_2 = r"https://api.hh.ru/vacancies"
-url_3 = r"https://www.booking.com/searchresults.ru.html?label=gen173nr-1FCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEU2AEB6AEB-AELiAIBqAIDuAKgxYXuBcACAQ&sid=bc5edb21df74621477bf9c71bcf97f49&sb=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.ru.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEU2AEB6AEB-AELiAIBqAIDuAKgxYXuBcACAQ%3Bsid%3Dbc5edb21df74621477bf9c71bcf97f49%3Bsb_price_type%3Dtotal%26%3B&ss=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C%2C+%D0%9F%D0%B5%D1%80%D0%BC%D1%81%D0%BA%D0%B8%D0%B9+%D0%BA%D1%80%D0%B0%D0%B9%2C+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&is_ski_area=&ssne=%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD&ssne_untouched=%D0%9B%D0%BE%D0%BD%D0%B4%D0%BE%D0%BD&checkin_year=2019&checkin_month=11&checkin_monthday=9&checkout_year=2019&checkout_month=11&checkout_monthday=10&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&ss_raw=gthvm&ac_position=0&ac_langcode=ru&ac_click_type=b&dest_id=-2980155&dest_type=city&iata=PEE&place_id_lat=58.01496&place_id_lon=56.24672&search_pageview_id=089a5390e2b60096&search_selected=true&search_pageview_id=089a5390e2b60096&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0"
-
+url_1 = r'https://ru.wikipedia.org/w/index.php?title=JSON&action=history'
+# url_2 = r'https://api.hh.ru/vacancies?industry=7&per_page=10&page=0'
+url_2 = r'https://api.hh.ru/vacancies'
+url_3 = r'https://www.booking.com/searchresults.ru.html?label=gen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEM2AED6AEBiAIBqAIDuAKz486LBsACAdICJDE3ZDhlZmZhLWI1ZTAtNDE3Yi04ODQxLTU0ODMxYjA2MzQzNNgCBOACAQ&sid=15910ac4ef1e864c27fdd2e87e03835b&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.ru.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaMIBiAEBmAEhuAEXyAEM2AED6AEBiAIBqAIDuAKz486LBsACAdICJDE3ZDhlZmZhLWI1ZTAtNDE3Yi04ODQxLTU0ODMxYjA2MzQzNNgCBOACAQ%3Bsid%3D15910ac4ef1e864c27fdd2e87e03835b%3Bsb_price_type%3Dtotal%3Bsig%3Dv10it45QJ4%26%3B&ss=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C&is_ski_area=0&ssne=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C&ssne_untouched=%D0%9F%D0%B5%D1%80%D0%BC%D1%8C&dest_id=-2980155&dest_type=city&checkin_year=2021&checkin_month=10&checkin_monthday=30&checkout_year=2021&checkout_month=10&checkout_monthday=31&group_adults=1&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&order=price&offset=0'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
 
 
@@ -19,13 +18,13 @@ def Task_1():
     regex = r'(?<!:)([0-9a-fA-F]{4}(:[0-9a-fA-F]{4}){7})(?!:)|(\b(?:(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:\.|$)){4}\b)'
 
     request = requests.get(url_1, headers=headers)
-    soup = BeautifulSoup(request.text, "html.parser")
+    soup = BeautifulSoup(request.text, 'html.parser')
 
     ip_list = []
     country = []
     tree_elements = soup.find_all('span', class_='history-user')
 
-    print("Парсинг сайта")
+    print('Парсинг сайта')
     for element in tree_elements:
         match = re.search(regex, element.find('a', class_='mw-userlink').get_text().strip())
         if match is not None:
@@ -34,14 +33,14 @@ def Task_1():
 
     # print(ip_list)
     # print(set(ip_list))
-    print("Поиск стран по ip")
+    print('Поиск стран по ip')
     for ip in set(ip_list):
-        request = requests.get(r"http://api.ipstack.com/" + ip + "?access_key=" + api_key)
+        request = requests.get(r'http://api.ipstack.com/' + ip + '?access_key=' + api_key)
         obj = request.json()
         # print(obj["country_name"])
-        country.append(obj["country_name"])
+        country.append(obj['country_name'])
 
-    print("---------------")
+    print('---------------')
     dictionary = {i: country.count(i) for i in country}
     for w in sorted(dictionary, key=dictionary.get, reverse=True):
         print(w, dictionary[w])
@@ -63,7 +62,7 @@ def Task_2():
         https://api.hh.ru/areas
         Россия - 113
         Пермь - 72
-        Пермский край -1317
+        Пермский край - 1317
         Москва - 1
         '''
         params = {
@@ -98,38 +97,64 @@ def Task_2():
         else:
             page += 1
 
-    print(f'Средняя зарплата Python разработчиков от {round(low_price/low_count)} до {round(high_price/high_count)}')
-    #request = requests.get(url_2)
-    #pprint(request.json())
+    print(
+        f'Средняя зарплата Python разработчиков от {round(low_price / low_count)} до {round(high_price / high_count)}')
+    # request = requests.get(url_2)
+    # pprint(request.json())
 
 
 def Task_3():
+    end = False
+    price = 0
+    offset = 0
+    today = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
+    saturday_delta = timedelta((7 + 5 - today.weekday()) % 7)
+    sunday_delta = timedelta((7 - 1 - today.weekday()) % 7)
+    url = url_3.replace('checkin_year=2021', 'checkin_year=' + str((today + saturday_delta).strftime('%Y'))) \
+        .replace('checkin_month=10', 'checkin_month=' + str((today + saturday_delta).strftime('%m'))) \
+        .replace('checkin_monthday=30', 'checkin_monthday=' + str((today + saturday_delta).strftime('%d'))) \
+        .replace('checkout_year=2021', 'checkout_year=' + str((today + sunday_delta).strftime('%Y'))) \
+        .replace('checkout_month=10', 'checkout_month=' + str((today + sunday_delta).strftime('%m'))) \
+        .replace('checkout_monthday=31', 'checkout_monthday=' + str((today + sunday_delta).strftime('%d')))
+
     while True:
 
-        request = requests.get(url_3, headers=headers)
-        soup = BeautifulSoup(request.content, "html.parser")
+        request = requests.get(url, headers=headers)
+        soup = BeautifulSoup(request.content, 'html.parser')
 
-        hotelBlock = soup.find_all("div", {"class": "sr_item"})
+        hotelBlock = soup.find_all('div',
+                                   class_='_fe1927d9e _0811a1b54 _a8a1be610 _022ee35ec b9c27d6646 fb3c4512b4 fc21746a73')
 
-        i = 0
+        print("\n----------------------------------------")
         for hotel in hotelBlock:
-            hotelName = hotel.find("span", {"class": "sr-hotel__name"}).get_text().strip()
-            print(hotelName)
+            if hotel is not None:
+                print(hotel.find('div', class_='fde444d7ef _c445487e2').get_text(),
+                      hotel.find("span", class_="fde444d7ef _e885fdc12").get_text())
+                price += int(
+                    hotel.find('span', class_='fde444d7ef _e885fdc12').get_text().replace(' ', '').replace('руб.', ''))
+        print("----------------------------------------")
 
-            print("\n Элементов на странице:" + " " + str(len(hotelBlock)))
+        elements = re.search(r'\d+', soup.find('h1', class_='_30227359d _0db903e42').get_text()).group()
 
-            nextPage = soup.find("a", title="Next page")
-            if nextPage is None:
-                nextPage = soup.find("a", title="Следующая страница")
+        if offset + 25 <= int(elements) and end is False:
+            print(f'Элементы {offset}-{offset + 25} из {elements}')
+            offset += 25
+            url = url.replace(('&offset=' + str(offset - 25)), ('&offset=' + str(offset)))
+        elif offset + 25 == int(elements) and end is False:
+            print(f'Элементы {offset}-{elements} из {elements}')
+            url = url.replace(('&offset=' + str(offset - 25)), ('&offset=' + str(int(elements) - 1)))
+            end = True
+        elif offset > int(elements) and end is False:
+            print(f'Элементы {offset}-{elements} из {elements}')
+            break
+        else:
+            print(f'Элементы {offset}-{elements} из {elements}')
+            break
 
-            if nextPage is not None:
-                url = "https://www.booking.com/" + nextPage.attrs["href"]
-                print("Обработка следующей страницы... \n")
-            else:
-                break
+    print(f"Средняя стоимость проживания с {(today + saturday_delta).strftime('%d.%m.%Y')} по {(today + sunday_delta).strftime('%d.%m.%Y')} в Перми = {round(price / int(elements))} руб.")
 
 
 if __name__ == '__main__':
     # Task_1()
-    Task_2()
-    # Task_3()
+    # Task_2()
+    Task_3()
