d578a23456e7880f4b636aa0aa895b8840febc10
Bloodies <eschepokov@mail.ru>
2022-03-01 21:16:06 +0500

    upd v0.1.3
    added multiprocessing

--- Graduate-work/src/scrapper/main.py
+++ Graduate-work/src/scrapper/main.py
@@ -3,11 +3,14 @@ import string
 import random
 import re
 import multiprocessing
+from multiprocessing.dummy import Pool as ThreadPool
 import threading
 import time
+import urllib3
 import os
 import datetime
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # id, название, возрастной рейтинг, длительность, год, страна, студия, бюджет, imdb популярность, сезон выхода, день выхода
 # imdb рейтинг, сборы в 1 уикенд, сборы в мире,
@@ -41,30 +44,66 @@ def saving(data):
             pass
         else:
             with open(f'imdb_set.txt', 'a', encoding='utf-8') as imdb_file:
-                imdb_file.write(f'{item["id"]};{item["name"]};{item["year"]};{item["url"]}\n')
+                imdb_file.write(f'{item["id"]}\t|\t{item["name"]}\t|\t{item["year"]}\t|\t{item["url"]}\n')
 
 
 def scraping_imdb():
-    counter = 1
-    # counter = 2198
+    pool = ThreadPool()
 
-    # s = time.time()
+    counter = 40001
+    # counter = 10001
+
+    # t = time.time()
     imdb_set = []
+    urls = []
 
     while counter <= 10872600:
         if len(str(counter)) < 7:
-            imdb_set.append(collect_movies('0' * (7 - len(str(counter))) + str(counter)))
+            for k in range(counter, counter + 10000, 1):
+                urls.append(f'https://www.imdb.com/title/tt{str("0" * (7 - len(str(k))) + str(k))}/')
         else:
-            imdb_set.append(collect_movies(counter))
+            for k in range(counter, counter + 10000, 1):
+                urls.append(f'https://www.imdb.com/title/tt{str(k)}/')
 
-        if counter % 100 == 0:
+        try:
+            imdb_set = pool.map(collect_movies, urls)
+        except (TimeoutError,
+                urllib3.exceptions.NewConnectionError,
+                urllib3.exceptions.MaxRetryError,
+                requests.exceptions.ConnectionError) as e:
+            print('\n\t\t\t\t\033[41m\033[30m\033[6m       SLEEP TIME       \033[0m\n')
+            time.sleep(10)
+            continue
+
+        if int(len(imdb_set)) % 10000 == 0:
             print('\n\t\t\t\t\033[31m\033[40m\033[6m\033[4m       SAVING DATA       \033[0m\n')
-            saving(imdb_set)
+
+            for item in imdb_set:
+                if item['name'] == 'skip':
+                    pass
+                else:
+                    with open(f'imdb_set.txt', 'a', encoding='utf-8') as imdb_file:
+                        imdb_file.write(f'{item["id"]}\t|\t{item["name"]}\t|\t{item["year"]}\t|\t{item["url"]}\n')
+
             imdb_set.clear()
+            urls.clear()
 
-        counter += 1
+        counter += 10000
 
-    # print(time.time() - s)
+    # while counter <= 10872600:
+    #     if len(str(counter)) < 7:
+    #         imdb_set.append(collect_movies(f'https://www.imdb.com/title/tt{str("0" * (7 - len(str(counter))) + str(counter))}/'))
+    #     else:
+    #         imdb_set.append(collect_movies(f'https://www.imdb.com/title/tt{str(counter)}/'))
+    #
+    #     if counter % 1 == 0:
+    #         print('\n\t\t\t\t\033[31m\033[40m\033[6m\033[4m       SAVING DATA       \033[0m\n')
+    #         saving(imdb_set)
+    #         imdb_set.clear()
+    #
+    #     counter += 1
+
+    # print('Elapsed {:.3f} secs'.format(time.time() - t))
 
 
 def skip(arg):
@@ -73,25 +112,28 @@ def skip(arg):
     return imdb_set
 
 
-def collect_movies(movie_id):
+def collect_movies(url):
     global collected_imdb_counter
     hour, minute, year, mpaa = '', '', '', ''
 
-    title_url = 'https://www.imdb.com/title/tt' + str(movie_id) + '/'
-    request = requests.get(title_url, headers=headers)
-    soup = BeautifulSoup(request.text, 'html.parser')
+    # title_url = f'https://www.imdb.com/title/tt{str(movie_id)}/'
+    session = requests.Session()
+    request = session.get(url, headers=headers)
+    soup = BeautifulSoup(request.content, 'lxml')
+
 
     success = soup.find('li', class_='ipc-inline-list__item')
     error_404_1 = soup.find('div', class_='error_code')
     error_404_2 = soup.find('div', class_='_error__QuoteBubbleText-sc-ql15x0-5')
 
+    movie_id = url.replace('https://www.imdb.com/title/tt', '').replace('/', '')
     title = soup.find('h1', class_='TitleHeader__TitleText-sc-1wu6n3d-0')
 
     if title is not None:
-        print(f'\033[33m {title.get_text()} \033[0m ({title_url}) '
+        print(f'\033[33m {title.get_text()} \033[0m ({url}) '
               f'[0{movie_id} out of 10872600] \033[32m{collected_imdb_counter}\033[0m found')
     else:
-        print(f'\033[31m No title \033[0m ({title_url}) '
+        print(f'\033[31m No title \033[0m ({url}) '
               f'[0{movie_id} out of 10872600] \033[32m{collected_imdb_counter}\033[0m found')
 
     if error_404_1 is not None:
@@ -102,10 +144,11 @@ def collect_movies(movie_id):
         if error_404_2.get_text() == '404 Error':
             return skip('ERROR 404')
 
-    if success.get_text() in ('TV Series', 'Video'):
-        return skip(success.get_text())
-    elif success.get_text()[:7] == 'Episode':
-        return skip('EP of TV Series')
+    if success is not None:
+        if success.get_text() in ('TV Series', 'Video'):
+            return skip(success.get_text())
+        elif success.get_text()[:7] == 'Episode':
+            return skip('EP of TV Series')
     elif success is None:
         return skip('No data')
 
@@ -124,22 +167,29 @@ def collect_movies(movie_id):
             year = re.findall(r'\d{4}', item.get_text()) if not year else year
             mpaa = re.findall(r'\b[0-9]{1,2}\+', item.get_text()) if not mpaa else mpaa
 
+    # box_office_items = soup.find('ul', class_='ipc-metadata-list').find_all('li', class_='ipc-metadata-list__item BoxOffice__MetaDataListItemBoxOffice-sc-40s2pl-2')
+    # print(box_office_items)
+    # print(soup.find('div', id_='__next'))
+    # for item in box_office_items:
+    #     print(1, item)
+
     if year in ('', None):
         return skip('No year')
 
-    hour = hour[0].replace('h', '')
-    minute = minute[0].replace('m', '')
+    if mpaa in ('', None):
+        return skip('No mpaa')
+
+    hour = hour[0].replace('h', '') if hour else 0
+    minute = minute[0].replace('m', '') if minute else 0
     year = year[0]
     mpaa = mpaa[0] if mpaa else 'Not rated'
-
-    print(hour, minute, year, mpaa)
 
     duration = int(hour) * 60 + int(minute)
     if int(duration) <= 40:
         return skip(f'duration < 40 [{duration}min]')
 
-    print(f'\t\t\033[32m{movie_id} {name} {year} {mpaa} {duration}min\033[0m')
-    imdb_set = {'url': title_url, 'id': movie_id, 'name': name, 'year': year}
+    print(f'\t\t\033[32m{url} {name} {year} {mpaa} {duration}min\033[0m')
+    imdb_set = {'url': url, 'id': movie_id, 'name': name, 'year': year}
     collected_imdb_counter = collected_imdb_counter + 1
     return imdb_set
 
